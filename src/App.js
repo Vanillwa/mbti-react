52d000168464f521f2b2df3ab063c61e17c8ae17
k1mminho <brightdeep@naver.com>
2024-04-16 15:20:52 +0900

    사이드바 컴포넌트 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Header from "./component/Header";
+import Sidebar from "./component/Sidebar";
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Sidebar/>
       <Routes>
-
+        
       </Routes>
     </div>
   );
