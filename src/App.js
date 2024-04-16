d094820a814a4618f8c521737b48c6bf0c354c5f
vanillwa <yjh30448813@gmail.com>
2024-04-16 14:48:47 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Main from "./pages/Main";
+import Header from "./component/Header";
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Routes>
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
