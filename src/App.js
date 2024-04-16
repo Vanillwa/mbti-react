5f554e04b9d14a6a99eba4d25b58a9c5203585e4
k1mminho <brightdeep@naver.com>
2024-04-16 16:45:39 +0900

    임시 왼쪽 사이드바

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import  Nav  from "./component/Nav";
+import LeftSidebar from "./component/LeftSidebar";
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <LeftSidebar/>
       <Routes>
         <Route path="/" element={<Main/>}></Route>
-      
+
       </Routes>
     </div>
   );
