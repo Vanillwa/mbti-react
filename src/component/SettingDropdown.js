0b9415ca3f0649ade2527e3fc05bdd3a9c0604f6
k1mminho <brightdeep@naver.com>
2024-04-23 11:27:39 +0900

    모달만드는중

--- src/component/SettingDropdown.js
+++ src/component/SettingDropdown.js
@@ -9,7 +9,7 @@ const SettingDropdown = () => {
 
   return (
     <div className={styles.menu}>
-      <div onClick={toggleDropdown}>설정</div>
+      <div type='button' onClick={toggleDropdown}>설정</div>
       <div className={`dropdown-content ${dropdown ? "show" : ""}`}>
         <Link to='/logout' onClick={closeDropdown}>로그아웃</Link>
         <div onClick={closeDropdown} type='button'>다크모드</div>
