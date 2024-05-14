3f05d9b5fae885b73f32de7b1637b4c998f1ce17
simdongjun1 <bbs1001kr@naver.com>
2024-05-14 14:44:02 +0900

    css

--- src/component/Alarm.js
+++ src/component/Alarm.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import {ToastContainer, toast } from 'react-toastify';
 import { socket } from '../service/socket/socket';
 
+
+
 const Alarm = () => {
 
   const notify = ()=> toast('메세지가 도착했습니다.')
