09be03bbfab954a4878c1b06852137bcd9bb35cd
k1mminho <brightdeep@naver.com>
2024-05-14 16:39:02 +0900

    오류 수정

--- src/component/Alarm.js
+++ src/component/Alarm.js
@@ -1,24 +1,42 @@
-import React, { useEffect } from 'react';
-import {ToastContainer, toast } from 'react-toastify';
-import { socket } from '../service/socket/socket';
+import React, { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { socket } from "../service/socket/socket";
 
-import 'react-toastify/dist/ReactToastify.css'
-import { useNavigate } from 'react-router-dom';
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
 const Alarm = () => {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
-  const notify = (data)=> {
-    
-    toast(`${data.sendUser.nickname} : ${data.message}`,{
-      autoClose : 2000,
-      position:"bottom-right",
+  const [userToastIds, setUserToastIds] = useState({});
+  const notify = (data) => {
+    if (userToastIds[data.sendUser.nickname]) {
+      toast.dismiss(userToastIds[data.sendUser.nickname]);
+    }
+
+    const toastId = toast(`${data.sendUser.nickname} : ${data.message}`, {
+      autoClose: 2000,
+      position: "bottom-right",
+      onClick: () => {
+        navigate(`/chat/list/${data.roomId}`);
+        toast.dismiss(toastId)
+      },
+    });
+    setUserToastIds({
+      ...userToastIds,
+      [data.sendUser.nickname] : toastId,
     })
-  } 
-    // socket.on('notification', notify)
+  };
 
-    return (
+  useEffect(() => {
+    socket.on("notification", notify);
+    return () => {
+      socket.off("notification", notify);
+    };
+  }, [userToastIds]);
+
+  return (
     <div>
-      <ToastContainer onClick={navigate(`/chat/list/`)}/>
+      <ToastContainer />
     </div>
   );
 };
