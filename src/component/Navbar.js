6f29028c136ed50b445fbfdc8b9ccebf1ea01a25
k1mminho <brightdeep@naver.com>
2024-05-21 16:42:30 +0900

    css수정중

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,7 +5,7 @@ import { useAuthContext } from "../context/AuthContext";
 import EListDropdown from "./EListDropdown";
 import IListDropdown from "./IListDropdown";
 
-import User from "../svg/people-fill.svg"
+import User from "../svg/people-fill.svg";
 import RUT from "../images/areyout.png";
 import write from "../svg/pencil-square.svg";
 import notImg from "../svg/person-circle.svg";
@@ -18,85 +18,90 @@ import { useNavigate } from "react-router-dom";
 import ProfileDropDown from "../component/ProfileDropDown";
 import sweetalert from "./sweetalert";
 import Alarm from "./Alarm";
+import home from "../svg/house.svg";
 
-
-const Navbar = ({chatData, chatStatus}) => {
+const Navbar = ({ chatData, chatStatus }) => {
   const { memoUserInfo } = useAuthContext();
   const [showDropdown, setShowDropdown] = useState(false);
   const { isLoggedIn, userInfo } = memoUserInfo;
 
-  const [showChat, setShowChat] = useState(false)
+  const [showChat, setShowChat] = useState(false);
 
   const navigate = useNavigate();
 
-  const handleopenModal = async() => {
-    const result = await sweetalert.question('로그인이 필요한 컨텐츠입니다.', '로그인 하시겠습니까?','로그인 하러가기','닫기')
-    if(result.dismiss) return;
-    else{
-      navigate('/')
+  const handleopenModal = async () => {
+    const result = await sweetalert.question(
+      "로그인이 필요한 컨텐츠입니다.",
+      "로그인 하시겠습니까?",
+      "로그인 하러가기",
+      "닫기"
+    );
+    if (result.dismiss) return;
+    else {
+      navigate("/");
     }
   };
 
-  const handleRequestLogin = ()=>{
-    navigate("/",{state:"login"})
-  }
-  
+  const handleRequestLogin = () => {
+    navigate("/", { state: "login" });
+  };
+
   const compareLogin = () => {
     if (isLoggedIn) {
       setShowDropdown(!showDropdown);
     } else {
-      navigate("/", { state: { state: "login" } }); 
+      navigate("/", { state: { state: "login" } });
     }
   };
 
-  const handleSearch =(e)=>{
-    e.preventDefault()
+  const handleSearch = (e) => {
+    e.preventDefault();
     let keyword = e.target.keyword.value;
 
     //if(keyword == "")return;
 
-    navigate("/search",{state:{keyword}})
-  }
+    navigate("/search", { state: { keyword } });
+  };
   useEffect(() => {
-    if(chatData != null){
-      if(isLoggedIn){
-        const allMessagesRead = chatData.every(item => item.unreadCount === 0);
+    if (chatData != null) {
+      if (isLoggedIn) {
+        const allMessagesRead = chatData.every(
+          (item) => item.unreadCount === 0
+        );
         setShowChat(!allMessagesRead);
       }
     }
-
-
   }, [chatData]);
 
-  if(chatStatus === 'loading'){
-    return(
-      <h1>로딩</h1>
-    )
+  if (chatStatus === "loading") {
+    return <h1>로딩</h1>;
   }
-  if(chatStatus === 'error'){
-    return(
-      <h1>에러</h1>
-    )
+  if (chatStatus === "error") {
+    return <h1>에러</h1>;
   }
 
-  console.log(chatData)
-  
-  
+  console.log(chatData);
+
   return (
     <div className={styles.container}>
-
       {/*왼쪽 사이드바*/}
       <div className={`${styles.item} ${styles.leftSidebar}`}>
         <div className={styles.menuItems}>
-        <Link to="/" className={styles.menu}>
-          <img src={RUT} className={styles.logo}></img>
-        </Link>
-        {/* <div className={styles.navbarSearch}>
+          <div className={styles.rutMenu}>
+            <Link to="/" className={`${styles.menu}`}>
+              <img src={RUT} className={styles.logo}/>
+            </Link>
+          </div>
+          <div className={styles.homeMenu}>
+            <Link to="/" className={`${styles.menu}`}>
+              <img src={home} className={styles.svg} />
+            </Link>
+          </div>
+          {/* <div className={styles.navbarSearch}>
           <form onSubmit={handleSearch}>
           <input type="text" placeholder="검색" name="keyword"  />
           </form>
         </div> */}
-          
 
           <Link className={styles.menu} to="/post/list">
             <img className={styles.svg} src={list} />
@@ -118,7 +123,8 @@ const Navbar = ({chatData, chatStatus}) => {
               <div
                 type="button"
                 className={styles.menu}
-                onClick={handleopenModal}>
+                onClick={handleopenModal}
+              >
                 <img className={styles.svg} src={write} />
                 <div className={styles.span}>글쓰기</div>
               </div>
@@ -135,77 +141,74 @@ const Navbar = ({chatData, chatStatus}) => {
               <div
                 type="button"
                 className={styles.menu}
-                onClick={handleopenModal}>
+                onClick={handleopenModal}
+              >
                 <img className={styles.svg} src={friend} />
                 <div className={styles.span}>친구</div>
               </div>
-              
             </div>
           )}
           {isLoggedIn ? (
             // 채팅창 이동
             <Link className={styles.menu} to="/chat/list">
-              <img className={styles.svg} src={chatting}/>
+              <img className={styles.svg} src={chatting} />
               <div className={styles.span}>채팅</div>
-              {showChat == true ? <div className={styles.showChat}>❕</div> : null}
-              
+              {showChat == true ? (
+                <div className={styles.showChat}>❕</div>
+              ) : null}
             </Link>
           ) : (
             <div>
               <div
                 type="button"
                 className={styles.menu}
-                onClick={handleopenModal}>
+                onClick={handleopenModal}
+              >
                 <img className={styles.svg} src={chatting} />
-                <div className={styles.span} >채팅</div>
+                <div className={styles.span}>채팅</div>
               </div>
             </div>
           )}
 
-
           {userInfo != null && userInfo.role === "admin" ? (
             <>
               <Link className={styles.menu} to={"/reportlist"}>
-              
                 <PiSirenFill className={styles.svg} />
-                <div className={styles.span}>
-                신고목록
-                </div>
+                <div className={styles.span}>신고목록</div>
               </Link>
               <Link className={styles.menu} to={"/user"}>
-                <img className={styles.svg} src={User}/>
-                <div className={styles.span}>
-                회원목록
-                </div>
+                <img className={styles.svg} src={User} />
+                <div className={styles.span}>회원목록</div>
               </Link>
             </>
           ) : (
             ""
           )}
           <div className={styles.menu}>
-          <button
-            className={styles.profileBtn}
-            type="button"
-            onClick={compareLogin}>
-            <div className={styles.span}>프로필</div>
-            {isLoggedIn ? (
-              <img src={userInfo.profileImage} className={`${styles.userImg} ${styles.svg}`} />
-            ) : (
-              <img src={notImg} alt="회원사진" className={styles.userImg}  />
-            )}
-            
-          </button>
-          {showDropdown && <ProfileDropDown/>}
+            <button
+              className={styles.profileBtn}
+              type="button"
+              onClick={compareLogin}
+            >
+              <div className={styles.span}>프로필</div>
+              {isLoggedIn ? (
+                <img
+                  src={userInfo.profileImage}
+                  className={`${styles.userImg} ${styles.svg}`}
+                />
+              ) : (
+                <img src={notImg} alt="회원사진" className={styles.userImg} />
+              )}
+            </button>
+            {showDropdown && <ProfileDropDown />}
+          </div>
         </div>
-        </div>
-
-    
       </div>
 
       {/* 컨텐츠 영역 */}
       <div className={`${styles.item} ${styles.content}`}>
         <Outlet />
-        <Alarm/>
+        <Alarm />
       </div>
     </div>
   );
