1ce857d530996c63c4cca5b7295dd551a0043d03
simdongjun1 <bbs1001kr@naver.com>
2024-05-21 15:46:56 +0900

    크롭

--- src/pages/MemberRevise.js
+++ src/pages/MemberRevise.js
@@ -215,8 +215,8 @@ function MemberRevise() {
             </div>
 
             {croppedImage && (
-  <div className='cropperbox'>
-    <div className="cropper-wrapper">
+  <div >
+    <div className="cropper-wrapper cropperBox ">
       <Cropper
         style={{ height: 400, width: '100%', }}
         aspectRatio={1}
@@ -234,9 +234,10 @@ function MemberRevise() {
         }}
       />
     </div>
-
+        <div className='cropperboxbtn'>
     <button type="submit" className='btn btn-primary mt-3 uploadbtn1'>업로드</button>
     <button type="button" className='btn btn-secondary mt-3 ml-2 uploadbtn2' onClick={cancel}> 뒤로가기</button> 
+  </div>
   </div>
 )}
              
