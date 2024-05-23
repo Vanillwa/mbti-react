8a93ef5ee959ca21016cfade0dca2a8fddabc44e
vanillwa <yjh30448813@gmail.com>
2024-05-23 17:15:29 +0900

    안쓰는거 삭제

--- src/pages/PostView.js
+++ src/pages/PostView.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ViewContent from "../component/ViewContent";
 import ViewComment from "../component/ViewComment";
+import styles from "../css/postView.module.css"
 
 function PostView() {
 
   return (
-    <>
+    <section className={styles.section}>
       <ViewContent/>
       <ViewComment/>
-    </>
+    </section>
   );
 }
 
