d3b2d01b6f5871aa325983dd3f5b04bc418cdeee
k1mminho <brightdeep@naver.com>
2024-05-27 11:47:31 +0900

    드롭다운 변경

--- src/component/IListDropdown.js
+++ src/component/IListDropdown.js
@@ -1,23 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { Dropdown } from "react-bootstrap";
 import styles from '../css/listDropdown.module.css'
 function IListDropdown() {
+  const [query, setQuery] = useSearchParams();
+  const mbti = query.get("mbti");
+  console.log(mbti)
+
+  const mbtiTypes = ['ESTJ', 'ESTP', 'ESFJ', 'ESFP', 'ENTJ', 'ENTP', 'ENFJ', 'ENFP', 'null'];
 
   return (<>
+  
     <Dropdown>
-      <Dropdown.Toggle variant="" id="dropdown-basic" className={styles.dropBtn}>
-        I 게시판
+      <Dropdown.Toggle variant="" id="dropdown-I" className={styles.dropBtn}>
+      {mbtiTypes.includes(mbti) ? 'I게시판' : mbti + '게시판'}
       </Dropdown.Toggle>
-      <Dropdown.Menu>
-        <Dropdown.Item><Link to='/post/list?mbti=ISTJ'>ISTJ</Link></Dropdown.Item>
-        <Dropdown.Item><Link to='/post/list?mbti=ISTP'>ISTP</Link></Dropdown.Item>
-        <Dropdown.Item><Link to='/post/list?mbti=ISFJ'>ISFJ</Link></Dropdown.Item>
-        <Dropdown.Item><Link to='/post/list?mbti=ISFP'>ISFP</Link></Dropdown.Item>
-        <Dropdown.Item><Link to='/post/list?mbti=INTJ'>INTJ</Link></Dropdown.Item>
-        <Dropdown.Item><Link to='/post/list?mbti=INTP'>INTP</Link></Dropdown.Item>
-        <Dropdown.Item><Link to='/post/list?mbti=INFJ'>INFJ</Link></Dropdown.Item>
-        <Dropdown.Item><Link to='/post/list?mbti=INFP'>INFP</Link></Dropdown.Item>
+      <Dropdown.Menu className={styles.menu}>
+        <Dropdown.Item className={styles.item} as={Link} to="/post/list?mbti=ISTJ">ISTJ</Dropdown.Item>
+        <Dropdown.Item className={styles.item} as={Link} to="/post/list?mbti=ISTP">ISTP</Dropdown.Item>
+        <Dropdown.Item className={styles.item} as={Link} to="/post/list?mbti=ISFJ">ISFJ</Dropdown.Item>
+        <Dropdown.Item className={styles.item} as={Link} to="/post/list?mbti=ISFP">ISFP</Dropdown.Item>
+        <Dropdown.Item className={styles.item} as={Link} to="/post/list?mbti=INTJ">INTJ</Dropdown.Item>
+        <Dropdown.Item className={styles.item} as={Link} to="/post/list?mbti=INTP">INTP</Dropdown.Item>
+        <Dropdown.Item className={styles.item} as={Link} to="/post/list?mbti=INFJ">INFJ</Dropdown.Item>
+        <Dropdown.Item className={styles.item} as={Link} to="/post/list?mbti=INFP">INFP</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
     </>
