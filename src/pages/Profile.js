99bef47cdcaf7cf9e9e0b5bb520b3adbb0ec8e61
simdongjun1 <bbs1001kr@naver.com>
2024-05-27 16:05:56 +0900

    수정

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { getProfileList } from "../service/api/postAPI";
 import styles from "../css/profile.module.css";
 import { Link, useParams } from "react-router-dom";
@@ -33,7 +32,7 @@ function Profile() {
     );
   }
 
-  if (data.length === 0) {
+  if (data.length == 0) {
     return (
       <div>
         <h1>작성된 글이 없습니다.</h1>
@@ -47,65 +46,64 @@ function Profile() {
     return htmlString.replace(/<[^>]*>?/gm, "");
   }
 
+  console.log(data)
   return (
     <>
       <div className={`container ${styles.container}`}>
-        <h2 className={styles.title}>{nickname}님의 게시글</h2>
+        <h2>{nickname}님의 게시글</h2>
       </div>
       <div className={`row ${styles.postBox}`}>
         {data.recentPost.map((userdata) => (
-          <div
-            className={`col col-3 ${styles.postWrap} ${data.recentPost.length === 1 ? styles.fullWidth : ""}`}
-            key={userdata.writerId}
-            style={{ display: "inline-block", verticalAlign: "top" }}
-          >
-            <Link
-              to={`/post/view?postId=${userdata.postId}`}
-              className={styles.postLink}
-            >
-              <div className={styles.postHeader}>
-                <img
-                  className={styles.img}
-                  src={
-                    userdata.content.match(
-                      /<img\s+[^>]*?src\s*=\\s*['"]([^'"]*?)['"][^>]*?>/
-                    )?.[1] || noImg
-                  }
-                  alt="post thumbnail"
-                />
-              </div>
-              <div className={styles.postBody}>
-                <div>
-                  <div className={styles.imgBox}>
-                    <div className={styles.thumbnail}></div>
-                    <div className={styles.dateReadhitBox}>
-                      <div className={styles.title}>{userdata.title}</div>
-                      <div className={styles.content}>
-                        {'\u00A0\u00A0'}
-                        {removeHTMLTags(userdata.content)}
-                      </div>
-                      <div className={styles.bottom}>
-                        <div>
-                          {new Date(userdata.createdAt).toLocaleDateString()}
-                        </div>
-                        <div className={styles.icone}>
-                          <div>
-                            <img src={like} alt="like" />
-                            {userdata.like}
-                          </div>
-                          <div>
-                            <img src={readhit} alt="readhit" />
-                            {userdata.readhit}
-                          </div>
-                        </div>
-                      </div>
+          
+          <Link to={`/post/view?postId=${userdata.postId}`} className={`col col-3 ${styles.postWrap}`} key={userdata.writerId}>
+            <div className={styles.postHeader}>
+              <img
+                className={styles.img}
+                src={
+                  userdata.content.match(
+                    /<img\s+[^>]*?src\s*=\s*['"]([^'"]*?)['"][^>]*?>/
+                  )?.[1] || noImg
+                }
+              />
+              
+            </div>
+            
+            <div className={styles.postBody}>
+            <div >
+                <div className={styles.imgBox}>
+                  <div className={styles.thumbnail}></div>
+                  <div className={styles.dateReadhitBox}>
+                    <div className={styles.title}> {userdata.title}</div>
+                    <div className={styles.content} >
+                    {'\u00A0\u00A0'}
+                      {removeHTMLTags(userdata.content)}
                     </div>
+
+                    <div className={styles.bottom}>
+                    <div>
+                    {new Date(userdata.createdAt).toLocaleDateString()}
+                    </div>
+                    <div className={styles.icone}>
+                    <div>
+                    <img src={like}/>
+                    {userdata.like}
+                    </div>
+                    <div>
+                    <img src={readhit}/>
+                    {userdata.readhit}
+                    </div>
+                    </div>
+                    
+
+                    </div>
+                    
                   </div>
-                  <div className={styles.titleBox}></div>
+                
                 </div>
+                <div className={styles.titleBox}></div>
               </div>
-            </Link>
-          </div>
+            </div>
+          </Link>
         ))}
       </div>
     </>
