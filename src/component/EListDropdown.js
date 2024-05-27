bce12bca5385f581383e1a8397c988882dfc26e4
k1mminho <brightdeep@naver.com>
2024-05-27 10:35:15 +0900

    e, i 드롭다운 부트스트랩으로 변견

--- src/component/EListDropdown.js
+++ src/component/EListDropdown.js
@@ -1,43 +1,27 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
+import Dropdown from 'react-bootstrap/Dropdown';
 import { Link } from "react-router-dom";
 import styles from '../css/listDropdown.module.css'
-import mbtiE from '../images/mbtiE.png'
 function EListDropdown() {
-  const [dropdown, setDropdown] = useState(false);
-  const toggleDropdown = () => setDropdown(!dropdown);
-  const closeDropdown = ()=>setDropdown(false)
 
+  return (<>
+    <Dropdown>
+      <Dropdown.Toggle variant="" id="dropdown-basic" className={styles.dropBtn}>
+        E 게시판
+      </Dropdown.Toggle>
 
-  const dropdownRef = useRef();
-
-  useEffect(()=>{
-    const handleClickOutside = (event) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        closeDropdown();
-      }
-    };
-    document.addEventListener('mousedown', handleClickOutside);
-    return()=>{
-      document.addEventListener('mousedown', handleClickOutside);
-
-    }
-  }, [dropdownRef])
-  return (
-    <div className={styles.listMenu} ref={dropdownRef}>
-      <div  className={`${styles.dropBtn} ${styles.link}`} onClick={toggleDropdown}>
-      <img className={styles.svg} src={mbtiE} /><div className={styles.span}>E게시판</div>
-      </div>
-      <div className={`${styles.dropdownContent} ${dropdown ? styles.show : ""}`}>
-        <Link to="/post/list?mbti=ESTJ" onClick={closeDropdown}>ESTJ</Link>
-        <Link to="/post/list?mbti=ESTP" onClick={closeDropdown}>ESTP</Link>
-        <Link to="/post/list?mbti=ESFJ" onClick={closeDropdown}>ESFJ</Link>
-        <Link to="/post/list?mbti=ESFP" onClick={closeDropdown}>ESFP</Link>
-        <Link to="/post/list?mbti=ENTJ" onClick={closeDropdown}>ENTJ</Link>
-        <Link to="/post/list?mbti=ENTP" onClick={closeDropdown}>ENTP</Link>
-        <Link to="/post/list?mbti=ENFJ" onClick={closeDropdown}>ENFJ</Link>
-        <Link to="/post/list?mbti=ENFP" onClick={closeDropdown}>ENFP</Link>
-      </div>
-    </div>
+      <Dropdown.Menu>
+        <Dropdown.Item><Link to='/post/list?mbti=ESTJ'>ESTJ</Link></Dropdown.Item>
+        <Dropdown.Item><Link to='/post/list?mbti=ESTP'>ESTP</Link></Dropdown.Item>
+        <Dropdown.Item><Link to='/post/list?mbti=ESFJ'>ESFJ</Link></Dropdown.Item>
+        <Dropdown.Item><Link to='/post/list?mbti=ESFP'>ESFP</Link></Dropdown.Item>
+        <Dropdown.Item><Link to='/post/list?mbti=ENTJ'>ENTJ</Link></Dropdown.Item>
+        <Dropdown.Item><Link to='/post/list?mbti=ENTP'>ENTP</Link></Dropdown.Item>
+        <Dropdown.Item><Link to='/post/list?mbti=ENFJ'>ENFJ</Link></Dropdown.Item>
+        <Dropdown.Item><Link to='/post/list?mbti=ENFP'>ENFP</Link></Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+    </>
   );
 }
 
--- src/component/IListDropdown.js
+++ src/component/IListDropdown.js
@@ -1,45 +1,26 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+import { Dropdown } from "react-bootstrap";
 import styles from '../css/listDropdown.module.css'
-import mbtiI from '../images/mbtiI.png'
-
 function IListDropdown() {
-  const [dropdown, setDropdown] = useState(false);
-  const toggleDropdown = () => setDropdown(!dropdown);
-  const closeDropdown = ()=>setDropdown(false)
 
-  const dropdownRef = useRef();
-
-  useEffect(()=>{
-    const handleClickOutside = (event) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        closeDropdown();
-        
-      }
-    };
-    document.addEventListener('mousedown', handleClickOutside);
-    return()=>{
-      document.addEventListener('mousedown', handleClickOutside);
-
-    }
-  }, [dropdownRef])
-  return (
-    <div className={styles.listMenu} ref={dropdownRef}>
-      <div className={`${styles.dropBtn} ${styles.link}`} onClick={toggleDropdown}>
-      <img className={styles.svg} src={mbtiI}/><div className={styles.span}>I게시판</div>
-      </div>
-      <div className={`${styles.dropdownContent} ${dropdown ? styles.show : ""}`}>
-        
-        <Link to="/post/list?mbti=ISTJ" onClick={closeDropdown}>ISTJ</Link>
-        <Link to="/post/list?mbti=ISTP" onClick={closeDropdown}>ISTP</Link>
-        <Link to="/post/list?mbti=ISFJ" onClick={closeDropdown}>ISFJ</Link>
-        <Link to="/post/list?mbti=ISFP" onClick={closeDropdown}>ISFP</Link>
-        <Link to="/post/list?mbti=INTJ" onClick={closeDropdown}>INTJ</Link>
-        <Link to="/post/list?mbti=INTP" onClick={closeDropdown}>INTP</Link>
-        <Link to="/post/list?mbti=INFJ" onClick={closeDropdown}>INFJ</Link>
-        <Link to="/post/list?mbti=INFP" onClick={closeDropdown}>INFP</Link>
-      </div>
-    </div>
+  return (<>
+    <Dropdown>
+      <Dropdown.Toggle variant="" id="dropdown-basic" className={styles.dropBtn}>
+        I 게시판
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item><Link to='/post/list?mbti=ISTJ'>ISTJ</Link></Dropdown.Item>
+        <Dropdown.Item><Link to='/post/list?mbti=ISTP'>ISTP</Link></Dropdown.Item>
+        <Dropdown.Item><Link to='/post/list?mbti=ISFJ'>ISFJ</Link></Dropdown.Item>
+        <Dropdown.Item><Link to='/post/list?mbti=ISFP'>ISFP</Link></Dropdown.Item>
+        <Dropdown.Item><Link to='/post/list?mbti=INTJ'>INTJ</Link></Dropdown.Item>
+        <Dropdown.Item><Link to='/post/list?mbti=INTP'>INTP</Link></Dropdown.Item>
+        <Dropdown.Item><Link to='/post/list?mbti=INFJ'>INFJ</Link></Dropdown.Item>
+        <Dropdown.Item><Link to='/post/list?mbti=INFP'>INFP</Link></Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+    </>
   );
 }
 
